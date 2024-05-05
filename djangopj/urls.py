15c00e0835c814d0f2a6b8b3d718a00f10d97991
saru1406 <y.s1406@ezweb.ne.jp>
2024-05-06 06:30:51 +0900

    login

--- djangopj/urls.py
+++ djangopj/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
